96ea738f8481775ffa2dd142b3fd97de027d5658
Maker0N <makerov@e1.ru>
2023-02-19 00:24:42 +0100

    add docker

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express")
-// const config = require('config')
+const config = require('config')
+const path = require('path')
 const cors = require("cors");
 const chalk = require("chalk")
 const routes = require('./routes')
 
 const server = express();
-const PORT = process.env.PORT || 8080
+const PORT = config.get('port') || 8080
 
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }))
-server.use("/api/v1", routes);
+server.use("/api/v1", routes)
+
+if (process.env.NODE_ENV === 'production') {
+  server.use('/', express.static(path.join(__dirname, 'build')))
+  const indexPath = path.join(__dirname, 'build', 'index.html')
+  server.get('*', (req, res) => {
+    res.sendFile(indexPath)
+  })
+  console.log(chalk.red('Production'))
+} else {
+  console.log(chalk.blue('Development'))
+}
 
 async function start() {
   try {
