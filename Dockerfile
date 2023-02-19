cd82ed35cd906f4c8b404c002795681852856b1f
Maker0N <makerov@e1.ru>
2023-02-19 20:23:36 +0100

    fix Dockerfile, remove console.log()

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ FROM node:18
 WORKDIR /app
 
 # Copy the application files into the working directory
-COPY . /index
+COPY . /app
 
 # Install the application dependencies
 RUN yarn install
+
+EXPOSE 3001
 
 # Define the entry point for the container
 CMD ["yarn", "server:build"]
--- routes/growatt.routes.js
+++ routes/growatt.routes.js
@@ -6,12 +6,10 @@ const router = express.Router({ mergeParams: true})
 router.post('/', async (req, res) => {
     const user = req.body.login
     const password = req.body.pass
-    console.log(req.body)
     const options = {}
     async function growattData() {
       const growatt = new Api({})
       const login = await growatt.login(user, password).catch((e) => { console.log(e) })
-      console.log(growatt)
       const getAllPlantData = await growatt.getAllPlantData(options).catch((e) => { console.log(e) })
       const data = JSON.stringify(getAllPlantData, null, ' ')
       return data
